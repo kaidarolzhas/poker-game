a71e915e7e0d9c7d0636eb01c35fe1d34da16d6b
kaidarolzhas <kaidarolzhas@gmail.com>
2025-05-09 13:38:58 +0500

    created project
    (cherry picked from commit 7b93d6bb29aa2ab5333c396b8a05508495445874)

---
+++ src/main/java/kz/kaidarolzhas/pokerbackend/PokerBackendApplication.java
@@ -0,0 +1,13 @@
+package kz.kaidarolzhas.pokerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerBackendApplication.class, args);
+    }
+
+}
