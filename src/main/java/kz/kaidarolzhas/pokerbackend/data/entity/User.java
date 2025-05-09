a0beeb577364aee6fa7264f6da7e86a2245fa096
sNurseit <nurseit.taimas@gmail.com>
2025-05-09 15:16:40 +0500

    add exception handler and ddl auto update

--- src/main/java/kz/kaidarolzhas/pokerbackend/data/entity/User.java
+++ src/main/java/kz/kaidarolzhas/pokerbackend/data/entity/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     @Column(name = "password", columnDefinition = "text")
     private String password;
 
-    @Column(name = "bank")
+    @Column(name = "bank", nullable = true)
     private Integer bank;
 
     @ManyToMany(fetch = FetchType.EAGER)
